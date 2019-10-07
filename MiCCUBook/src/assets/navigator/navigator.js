4b38b6f9ee182cd732791f4f41c17fbae53bd3fb
packerfan626 <dori_mouawad@hotmail.com>
2019-10-06 21:29:42 -0700

    Updated Navigator to have a bottom tab navigator with basic icons

--- MiCCUBook/src/assets/navigator/navigator.js
+++ MiCCUBook/src/assets/navigator/navigator.js
@@ -1,14 +1,64 @@
+// Navigation imports
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import HomeView from '@views/HomeView';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
-const AppNavigator = createStackNavigator ({
+// Imported Views
+import HomeView from '@views/HomeView';
+import AboutView from '@views/AboutView';
+import CalculatorView from '@views/CalculatorView';
+
+// Components
+import { Icon } from 'native-base';
+import * as React from 'react';
+
+const BottomTabNavigator = createBottomTabNavigator ({
+    Calculator: {
+        screen: CalculatorView
+    },
     Home: {
+        screen: HomeView
+    },
+    Download: {
         screen: HomeView
     },
 }, {
     headerMode: 'none',
-    initialRouteName: 'Home'
+    initialRouteName: 'Home',
+    defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state;
+
+            if (routeName==='Calculator') {
+                return (
+                    <Icon name='calculator' size={40} style={{color:tintColor}}/>
+                )
+            }
+
+            if (routeName==='Home') {
+                return (
+                    <Icon name='home' size={40} style={{color:tintColor}}/>
+                )
+            }
+
+            if (routeName==='Download') {
+                return (
+                    <Icon name='download' size={40} style={{color:tintColor}}/>
+                )
+            }
+        }
+    })
+})
+
+const MainAppNavigator = createStackNavigator ({
+    BottomTabs: {
+        screen: BottomTabNavigator
+    },
+    About: {
+        screen: AboutView
+    },
+}, {
+    headerMode: 'none',
 });
 
-export default createAppContainer(AppNavigator);
+export default createAppContainer(MainAppNavigator);
