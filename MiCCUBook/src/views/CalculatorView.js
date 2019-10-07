2807222b9c10f89e4e855e54b23c257ca2b35005
packerfan626 <dori_mouawad@hotmail.com>
2019-10-06 21:30:20 -0700

    Created basic calculator view

---
+++ MiCCUBook/src/views/CalculatorView.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { 
+    Container,
+    Header,
+    Left,
+    Body,
+    Right,
+    Button,
+    Icon,
+    Title,
+    Text,
+    List,
+    ListItem,
+    Content
+ } from 'native-base';
+ import { Platform, StyleSheet } from 'react-native';
+ import colors from '@assets/colors';
+
+export default class CalculatorView extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+          <Container>
+              <Content>
+                  <Text>CalculatorView</Text>
+              </Content>
+          </Container>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: colors.TOP_HEADER_BACKGROUND,
+    },
+    header_title_text: {
+        color:colors.TOP_HEADER_TEXT,
+    },
+    header_icons: {
+        color:colors.TOP_HEADER_ICONS,
+    }
+})
